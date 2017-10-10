f752dcf7d5e57b0a8000bd76802c5962948f282f
broodmeister <isaiahchrst80@gmail.com>
2017-10-10 23:45:47 +0000

    Completed URL Shortening Part 1.

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8080; // default port 8080
 
 const urlDatabase = {
@@ -8,6 +9,20 @@ const urlDatabase = {
 };
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  let newURL = "";
+  const dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  do {
+    for (let i = 0; i < 6; i++ ) {
+      newURL += dictionary.charAt(
+        Math.round(Math.random() * dictionary.length));
+    }
+  } while(urlDatabase[newURL])
+  return newURL;
+}
 
 app.get("/", (req, res) => {
   res.end("Hello!\n");
@@ -27,9 +42,20 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body.longURL);
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase);
+  res.send("Ok");
 });
 
 app.listen(PORT, () => {
