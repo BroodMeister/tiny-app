0e18db5707ecd25feff71da30cc0ce8abf995c76
broodmeister <isaiahchrst80@gmail.com>
2017-10-10 22:54:12 +0000

    Completed Template Engine.

--- express_server.js
+++ express_server.js
@@ -1,19 +1,32 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
-  res.end("Hello!");
+  res.end("Hello!\n");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id , urls: urlDatabase};
+  res.render("urls_show", templateVars);
+});
+
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
