cae8db86870f011a8cb2375e695ff6fcb45f05f1
broodmeister <isaiahchrst80@gmail.com>
2017-10-11 23:51:25 +0000

    Tidied up code and fixed error that wouldn't load login information.

--- express_server.js
+++ express_server.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 
 const app = express();
 app.set('view engine', 'ejs');
-
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
+const PORT = process.env.PORT || 8080; // default port 8080
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -57,10 +57,6 @@ app.get("/u/:shortURL", (req,res) => {
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/u/:shortURL", (req,res) => {
-  res.redirect(urlDatabase[req.params.shortURL]);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
